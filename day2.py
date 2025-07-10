3f3ba1fb75de5c5490bca376c60c20ed8d18f9b4
Purnima2005 <purnimathp77@gmail.com>
2025-07-10 13:51:46 +0545

    assignnn

---
+++ day2.py
@@ -0,0 +1,16 @@
+#Slicing
+# name = "Purnima"
+# print (name[0:len(name)-2])
+
+#Length
+# print (len(name))
+
+#find and replace
+# name="sangeet"
+# print(name.find("g"))
+# replaced = name.replace("g","p")
+# print (replaced)
+
+
+text = "My name is Purnima \n\t and \n I am you can't imagine years old."
+print(text)
